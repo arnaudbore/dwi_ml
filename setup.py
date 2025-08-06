beb77738260da2d17a1ca708350acf9ccc3c97fa
arnaudbore <arnaud.bore@gmail.com>
2025-08-06 11:26:08 -0400

    try to update to pyproject

--- setup.py
+++ setup.py
@@ -14,35 +14,4 @@ with open('requirements.txt') as f:
         else:
             external_dependencies.append(dependency)
 
-
-# Get version and release info, which is all stored in version.py
-ver_file = os.path.join('dwi_ml', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=find_packages(),
-            python_requires=PYTHON_VERSION,
-            setup_requires=['numpy'],
-            install_requires=external_dependencies,
-            entry_points={
-                'console_scripts': ["{}=scripts_python.{}:main".format(
-                    os.path.basename(s),
-                    os.path.basename(s).split(".")[0]) for s in PYTHON_SCRIPTS]
-            },
-            scripts=[s for s in BASH_SCRIPTS],
-            data_files=[],
-            include_package_data=True)
-
-setup(**opts)
+setup(install_requires=external_dependencies)
